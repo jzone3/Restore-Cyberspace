7cf2d31d431fbe419d6423faf5acffc34b9b0c48
Jared Zoneraich <jszoneraich@gmail.com>
2014-09-14 03:49:17 -0400

    debug=False

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def share():
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
-	app.run(host='0.0.0.0', port=port,debug=True)
+	app.run(host='0.0.0.0', port=port,debug=False)
